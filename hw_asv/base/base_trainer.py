be10507d30930adb96cec7934aaf3773c8d37227
mac-mvak <maximusev1290@gmail.com>
2023-12-16 21:50:09 +0300

    change trainer

--- hw_asv/base/base_trainer.py
+++ hw_asv/base/base_trainer.py
@@ -176,8 +176,8 @@ class BaseTrainer:
 
         # load optimizer state from checkpoint only when optimizer type is not changed.
         if (
-                checkpoint["config"]["optimizer"] != self.config["optimizer"] or
-                checkpoint["config"]["lr_scheduler"] != self.config["lr_scheduler"]
+                checkpoint["config"]["optimizer"] != self.config["optimizer"] #or
+                #checkpoint["config"]["lr_scheduler"] != self.config["lr_scheduler"]
         ):
             self.logger.warning(
                 "Warning: Optimizer or lr_scheduler given in config file is different "
