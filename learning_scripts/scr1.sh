2f522af80be1467acb7710022a2ba9cb1443c1a5
mac-mvak <maximusev1290@gmail.com>
2023-12-16 07:03:10 +0300

    minor fix

--- learning_scripts/scr1.sh
+++ learning_scripts/scr1.sh
@@ -1,3 +1,3 @@
 python3 train.py -c final_data/config_rawnet_paper_params.json
 python3 train.py -c final_data/config_rawnet_no_bn.json
-python3 train.py -c final_data/config_rawnet _layer_gpu.json
+python3 train.py -c final_data/config_rawnet _layer_gru.json
